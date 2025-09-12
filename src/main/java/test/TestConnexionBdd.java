5be7efc50337f9b1a95f6e3c54fe6d03f6fcc59c
EstelleBihel <estelle.bihel@sts-sio-caen.info>
2025-09-12 11:57:36 +0200

    modif bdd

--- src/main/java/test/TestConnexionBdd.java
+++ src/main/java/test/TestConnexionBdd.java
@@ -16,8 +16,11 @@ public class TestConnexionBdd {
         System.out.println ("nombre de chevaux = " + DaoCheval.getLesChevaux(cnx).size());
 
         for (Cheval c : DaoCheval.getLesChevaux(cnx)) {
-            System.out.println("cheval : "+" "+c.getId()+" "+c.getNom()+" "+c.getRace().getNom()+" "+c.getSire()+" "+c.getDateNaissance()+" "+c.getSexe());
+            System.out.println("cheval : "+" "+c.getId()+" "+c.getNom()+" "+c.getRace().getNom()+" "+c.getSire()+" "+c.getDateNaissance()+" "+c.getSexe()+" "+c.getRobe());
         }
+
+        Cheval ch = DaoCheval.getLeCheval(cnx, 3);
+        System.out.println("cheval : "+" "+ch.getId()+" "+ch.getNom()+" "+ch.getRace().getNom()+" "+ch.getSire()+" "+ch.getDateNaissance()+" "+ch.getSexe()+" "+ch.getPoids()+" "+ch.getTaille()+" "+ch.getRobe());
 
 
 
